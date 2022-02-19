8912524eb21a1cb969233b22b58c81d3ceb5483c
Francisco Zanfranceschi <zanfranceschi@gmail.com>
2022-02-19 00:05:00 -0300

    teste

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿
-Console.WriteLine("Hello, World!");
+public class Program 
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Oi, mundo...");
+        Console.ReadLine();
+    }
+}
+
+
